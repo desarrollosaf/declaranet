4701902077de7b54a98e677db15d4c7fec6258e2
EstefaniaKuri <fannykury21@hotmail.com>
2020-10-06 12:43:39 -0500

    vistas y controladores

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+//Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource("/DatosDeclarante", "DatosGeneralesDeclaranteController")->name('*', 'datosDeclarante');
+Route::resource("/domicilioDeclarante", "DomicilioDeclaranteController")->name('*', 'domicilioDeclarante');
