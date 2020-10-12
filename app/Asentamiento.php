afbdb9cadf42a3ac33a1a6be0ef0130d9631c497
gis_a <gis_a@DESKTOP-CV8CQIO>
2020-10-12 10:38:05 -0500

    Modelos de Asentamientos y FormasPagos

--- app/Asentamiento.php
+++ app/Asentamiento.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asentamiento extends Model
 {
-    //
+    protected $table = "asentamientos";
+    protected $guarded = ["id","created_at","updated_at","deleted_at"];
 }
