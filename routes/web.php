71c9fed9466434f92e832358bc556e734843bebc
oscartic98 <tic.oscarrom@gmail.com>
2020-10-19 11:10:26 -0500

    Ruta experiencia laboral

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,6 @@ Route::resource("/datos_curriculares_declarante", "DatosCurricularesDeclaranteCo
 Route::resource("/datos_empleo_declarante", "DatosEmpleoDeclaranteController")->name('*', 'datos_empleo_declarante');
 Route::resource("/datos_pareja_declarante", "DatosParejaController")->name('*', 'datos_pareja_declarante');
 Route::resource("/datos_dependiente_declarante", "DatosDependienteEconomicoController")->name('*', 'datos_dependiente_declarante');
-Route::resource("/experiencia_laboral", "DatosDependienteEconomicoController")->name('*', 'experiencia_laboral');
+Route::resource("/experiencia_laboral", "ExperieciaLaboralController")->name('*', 'experiencia_laboral');
 Route::resource("/ingreso_neto", "IngresoNetoDeclaranteController")->name('*', 'ingreso_neto');
 Route::resource("/servidor_publico", "ServidorPublicoController")->name('*', 'servidor_publico');
