2bf488d5b45a49f5c5cf32dd1a4b7a4881328864
gis_a <gis_a@DESKTOP-CV8CQIO>
2020-10-19 11:24:39 -0500

    Controladores para administración de datos generales

---
+++ app/Http/Controllers/EstatusDeclaracionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EstatusDeclaracion;
+use Illuminate\Http\Request;
+
+class EstatusDeclaracionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EstatusDeclaracion  $estatusDeclaracion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EstatusDeclaracion $estatusDeclaracion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EstatusDeclaracion  $estatusDeclaracion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EstatusDeclaracion $estatusDeclaracion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EstatusDeclaracion  $estatusDeclaracion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EstatusDeclaracion $estatusDeclaracion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EstatusDeclaracion  $estatusDeclaracion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EstatusDeclaracion $estatusDeclaracion)
+    {
+        //
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TipoMovimiento;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tipo_movimientos = TipoMovimiento::all();
+        $declaraciones = [];
+        return view('home', compact("declaraciones","tipo_movimientos"));
     }
 }
