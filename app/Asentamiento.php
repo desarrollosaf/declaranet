109b300c05908b2dc3f16ca7fcc2f11d94a2cf91
gis_a <gis_a@DESKTOP-CV8CQIO>
2020-10-28 11:05:13 -0600

    Pruebas de slack

--- app/Asentamiento.php
+++ app/Asentamiento.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asentamiento extends Model
 {
+    
     protected $table = "asentamientos";
     protected $guarded = ["id","created_at","updated_at","deleted_at"];
 }
