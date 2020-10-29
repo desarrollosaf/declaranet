918404a7b7acd4732c2914713e12c7e37a802c9d
gis_a <gis_a@DESKTOP-CV8CQIO>
2020-10-29 11:48:23 -0600

    Catalogo de asentamientos

---
+++ database/migrations/2020_10_09_213022_create_asentamientos_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateAsentamientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asentamientos', function (Blueprint $table) {
+            $table->bigIncrements("id");
+            $table->string("cve_asenta");
+            $table->string("codigo_postal");
+            $table->string("asentamiento");
+            $table->unsignedBigInteger("municipio_id");
+            $table->foreign("municipio_id")->references("id")->on("municipios");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        $path = base_path("database/catalogos/catalogos/json/asentamientos.json");
+        $json = json_decode(file_get_contents($path));
+        foreach($json as $asentamiento){
+            DB::table("asentamientos")->insert([
+                "cve_asenta" => $asentamiento->cve_asenta,
+                "codigo_postal" => $asentamiento->nom_clave,
+                "asentamiento" => $asentamiento->nom_asenta,
+                "municipio_id" => 15
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asentamientos');
+    }
+}
