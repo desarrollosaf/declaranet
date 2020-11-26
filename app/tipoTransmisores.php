050455ddb00122d0d918ea272966530f0293f480
LEGISLATURA <LEGISLATURA@DESKTOP-S30KVKI>
2020-11-26 11:02:54 -0600

    Cambios en Bienes inmuebles

--- app/tipoTransmisores.php
+++ app/tipoTransmisores.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class tipoTransmisores extends Model
 {
-    protected $table = "regimenfiscals";
+    protected $table = "regimenfiscales";
     protected $guarded = ["id","created_at","updated_at","deleted_at"];
 }
