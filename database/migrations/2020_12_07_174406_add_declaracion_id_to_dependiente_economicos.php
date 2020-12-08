4e4f4a2de368edf2914253fd76f2d21e2b6c8103
Eduardo <Eduardo@DESKTOP-4SL803M>
2020-12-07 18:27:32 -0600

    Dependientes economicos

---
+++ database/migrations/2020_12_07_174406_add_declaracion_id_to_dependiente_economicos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDeclaracionIdToDependienteEconomicos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dependiente_economicos', function (Blueprint $table) {
+            $table->unsignedBigInteger('declaracion_id');
+            $table->foreign('declaracion_id')->references('id')->on('declaraciones');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('dependiente_economicos', function (Blueprint $table) {
+            $table->dropColumn('declaracion_id');
+        });
+    }
+}
