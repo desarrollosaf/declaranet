2f480d6444f329a204b9c5870dd1d4dfa3f44231
MarSGlez <martin.sanchezg@hotmail.com>
2020-12-10 12:57:43 -0600

    Adeudos completo

--- resources/views/adeudosDeclarante/create.blade.php
+++ resources/views/adeudosDeclarante/create.blade.php
@@ -2,9 +2,40 @@
 @section('content')
     <div class="card">
         <div class="card-header">
-            <h1>ADEUDOS / PASIVOS</h1>
-            <h6>(A la fecha de ingreso)</h6>
+            <div class="float-left">
+                <h1>ADEUDOS / PASIVOS </h1>
+                <h6>(A la fecha de ingreso)</h6>
+            </div>
+            <div class="float-right">
+                <span class="badge badge-success ml-auto px-4" data-toggle="collapse" href="#collapseAdeudos" role="button" aria-expanded="false" aria-controls="collapseExample"> AYUDA</span>
+            </div>
         </div>
+        <div class="collapse" id="collapseAdeudos">
+            <div class="card card-body">               
+                <ol>
+                    <li><strong>Titular del adeudo.</strong> Seleccionar alguna de las opciones del catálogo desplegable. En caso de señalar codeudor deberá indicar el nombre del tercero o terceros.</li>
+                    <li><strong>Tipo de adeudo.</strong> Seleccionar cualquiera de las siguientes opciones: crédito hipotecario, crédito automotriz, crédito personal, tarjeta de crédito bancaria, tarjeta de crédito departamental y préstamo personal, otros.<br>
+                        Se entenderá por crédito personal, aquel obtenido ante una institución de crédito o bancaria.<br>
+                        Se entenderá por préstamo personal, aquel obtenido a través de un familiar, amigo y/o conocido del Declarante.
+                    </li>
+                    <li><strong>Codeudor.</strong> En caso de ser codeudor con un tercero, seleccionar si es persona física o persona moral.</li>
+                    <li><strong>Nombre del codeudor.</strong> En caso de señalar codeudor, escribir el nombre, así como apellidos completos sin abreviaturas y sin acentos ni signos especiales. Si se trata de persona moral deberá proporcionar la denominación o razón social de la institución tal y como se encuentra en el documento con el que se acredita la adquisición.</li>
+                    <li><strong>RFC.</strong> En caso de personas físicas proporcionar los trece dígitos. En caso de personas morales establecer los doce dígitos.</li>
+                    <li><strong>Número de cuenta o contrato.</strong> Señalar el número de contrato, cuenta o cualquier dato que permita su identificación, en caso de préstamo personal se indicará no aplica.</li>
+                    <li><strong>Fecha de adquisición del adeudo/pasivo.</strong> Indicar la fecha en que adquirió el adeudo.</li>
+                    <li><strong>Monto original del adeudo/pasivo.</strong> Es necesario capturar cantidades sin comas, sin puntos, sin centavos y sin ceros a la izquierda. El monto a reportar será el inicial al adeudo.</li>
+                    <li><strong>Tipo de moneda.</strong> Seleccionar la moneda relativa al adeudo.</li>
+                    <li><strong>Saldo insoluto.</strong> Es necesario capturar cantidades sin comas, sin puntos, sin centavos y sin ceros a la izquierda.</li>
+                    <li><strong>Otorgante del crédito.</strong> Seleccionar si es persona física o persona moral.</li>
+                    <li><strong>Nombre, Institución o razón social (si aplica).</strong> Señalar el nombre de la institución donde contrajo la deuda.</li>
+                    <li><strong>RFC.</strong> En caso de personas físicas proporcionar los trece dígitos. En caso de personas morales establecer los doce dígitos.</li>
+                    <li><strong>Dónde se localiza el adeudo.</strong> Indicar si se encuentra en México o en el extranjero, debiendo señalar el país donde se localiza.</li>
+                </ol>
+
+                <strong>Aclaraciones/observaciones.</strong> En este espacio podrá realizar las aclaraciones u observaciones que considere pertinentes respecto de alguno o algunos de los incisos de este apartado.
+
+          </div>
+      </div>
         <div class="card-body">
             <div class="alert alert-danger" role="alert">
                 Todos los datos de los adeudos / pasivos a nombre de la pareja,
--- resources/views/apoyoBeneficiosDeclarante/create.blade.php
+++ resources/views/apoyoBeneficiosDeclarante/create.blade.php
@@ -3,9 +3,30 @@
     <div class="container">
         <div class="card mb-5 shadow-sm border-0 shadow-hover">
             <div class="card-header">
-                <h1>APOYO O BENEFICIO DECLARANTE</h1>
-                <h6>(Hasta los últimos dos años)</h6>
+                <div class="float-left">
+                    <h1>APOYO O BENEFICIO DECLARANTE</h1>
+                    <h6>(Hasta los últimos dos años)</h6>
+                </div>
+                <div class="float-right">
+                    <span class="badge badge-success ml-auto px-4" data-toggle="collapse" href="#collapseApoyo" role="button" aria-expanded="false" aria-controls="collapseExample"> AYUDA</span>
+                </div>
             </div>
+            <div class="collapse" id="collapseApoyo">
+                <div class="card card-body">               
+                    <ol>
+                        <li><strong>Titular del apoyo o beneficio.</strong> Son las personas que reciben el apoyo o beneficio público. De las opciones del catálogo indicar quien o quienes son los beneficiarios, señalar las veces que sean necesarias.</li>
+                        <li><strong>Nombre del programa.</strong> Proporcionar el nombre del programa por el cual recibe el apoyo o beneficio público.</li>
+                        <li><strong>Institución que otorga el apoyo.</strong> Señalar el nombre de la institución que le esté dando el apoyo.</li>
+                        <li><strong>Nivel u orden de gobierno.</strong> Seleccionar el orden de gobierno por medio del cual recibe el apoyo o beneficio público: federal, estatal o municipal/alcaldía.</li>
+                        <li><strong>Tipo de apoyo.</strong> Seleccionar del listado desplegable: subsidio (Ayuda económica que una persona recibe de un organismo oficial para satisfacer una necesidad determinada), servicio (Actividad o asistencia de un Ente Público, en favor del Declarante), Obra (materiales o servicios a favor del Declarante para construcción), otro, especifique.</li>
+                        <li><strong>Forma de recepción del beneficio.</strong> Señalar si el apoyo recibido es monetario o en especie, en caso de haber seleccionado monetario deberá seleccionar el tipo de moneda.</li>
+                        <li><strong>Especifique el apoyo.</strong> En caso de haber seleccionado en especie deberá describir en qué consiste el apoyo o beneficio.</li>
+                        <li><strong>Monto mensual aproximado del beneficio.</strong> Es necesario capturar cantidades sin comas, sin puntos, sin centavos y sin ceros a la izquierda. Indicar cantidad mensual del apoyo, si este no fuera mensual, realizar la operación aritmética a efecto de establecer la cantidad como lo solicita el formato.</li>
+                    </ol>
+                    <strong>Aclaraciones/observaciones.</strong> En este espacio podrá realizar las aclaraciones u observaciones que considere pertinentes respecto de alguno o algunos de los incisos de este apartado.
+    
+              </div>
+          </div>
             <div class="card-body">
                 {!! Form::open(['route'=>'apoyo_beneficio.store', 'method'=>'POST', 'files' => true, 'role' => 'form', 'id' => '']) !!}
                 @include('apoyoBeneficiosDeclarante.form')
