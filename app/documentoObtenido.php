0aadb49c9889738e868cad699e6e547a2a959e78
Eduardo <Eduardo@DESKTOP-4SL803M>
2020-12-10 16:28:35 -0600

    Se corrigen errores de experiencia laboral y datos curriculares

--- app/documentoObtenido.php
+++ app/documentoObtenido.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class documentoObtenido extends Model
 {
-   protected $table = "documento_obtenidos";
+   protected $table = "documentos_obtenidos";
    protected $guarded = ["id","created_at","updated_at","deleted_at"];
 }
