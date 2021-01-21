715e0ae4e94450bec6de28945ebf92ecbea32954
fanny <fannykury21@hotmail.com>
2021-01-21 14:05:52 -0600

    Vista viajes

--- resources/views/viajes/form.blade.php
+++ resources/views/viajes/form.blade.php
@@ -5,17 +5,22 @@
         <span class="text-danger" style="font-size:150%"></span>
     </div>
     <div class="form-group col-md-4">
+        <strong>{!! Form::label('otroEspecifique', 'Especifique : *') !!}</strong>
+        {!! Form::text("viajes[otro]", (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'ESPECIFIQUE',  'id' => 'otroEspecifique']) !!}
+        <span class="text-danger" style="font-size:150%"></span>
+    </div>
+    <div class="form-group col-md-4">
         <strong>{!! Form::label('nombreTercero', 'Nombre del tercero o terceros : *') !!}</strong>
         {!! Form::text("viajes[nombre]", (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'',  'id' => 'nombreTercero']) !!}
         <span class="text-danger" style="font-size:150%"></span>
     </div>
+</div>
+<div class="form-row">
     <div class="form-group col-md-4">
         <strong>{!! Form::label('tipoTercero', 'Tipo del tercero : *') !!}</strong>
         {!! Form::select("viajes[tipo]",$tipoTercero, (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'SELECCIONA UNA OPCIÓN',  'id' => 'tipoTercero']) !!}
         <span class="text-danger" style="font-size:150%"></span>
     </div>
-</div>
-<div class="form-row">
     <div class="form-group col-md-4">
         <strong>{!! Form::label('rfcTercero', 'RFC del tercero o terceros : *') !!}</strong>
         {!! Form::text("viajes[rfc]", (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'',  'id' => 'rfcTercero']) !!}
@@ -26,13 +31,13 @@
         {!! Form::text("viajes[monto]", (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>' $000.000',  'id' => 'montoViaje']) !!}
         <span class="text-danger" style="font-size:150%"></span>
     </div>
+</div>
+<div class="form-row">
     <div class="form-group col-md-4">
         <strong>{!! Form::label('tipoMoneda', 'Tipo moneda : *') !!}</strong>
         {!! Form::select("viajes[tipo]", $tipoMoneda, (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'SELECCIONA UNA OPCIÓN',  'id' => 'tipoMoneda']) !!}
         <span class="text-danger" style="font-size:150%"></span>
     </div>
-</div>
-<div class="form-row">
     <div class="form-group col-md-4">
         <strong>{!! Form::label('lugarViaje', 'Lugar del viaje : *') !!}</strong>
         {!! Form::select("viajes[lugar]", $lugarViaje, (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'SELECCIONA UNA OPCIÓN',  'id' => 'lugarViaje']) !!}
@@ -43,6 +48,8 @@
         {!! Form::select("viajes[motivo]", $motivoViaje, (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'SELECCIONE UNA OPCIÓN',  'id' => 'motivoViaje']) !!}
         <span class="text-danger" style="font-size:150%"></span>
     </div>
+</div>
+<div class="form-row">
     <div class="form-group col-md-4">
         <strong>{!! Form::label('otroEspecifique', 'Especifique : *') !!}</strong>
         {!! Form::text("viajes[otro]", (isset($viajes->tipo_donativos_id)) ? $donativo->tipo_donativos_id : null,['class'=>'form-control alert-danger text-uppercase', 'placeholder'=>'ESPECIFIQUE',  'id' => 'otroEspecifique']) !!}
