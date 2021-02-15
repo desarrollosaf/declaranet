a4337bbd146c3e9cef35a8b90a5855e015328255
giss <gis_ale13@hotmail.com>
2021-02-15 13:16:53 -0600

    experiencia

--- resources/views/experienciaLaboral/index.blade.php
+++ resources/views/experienciaLaboral/index.blade.php
@@ -67,7 +67,7 @@
                             </center>
                         </div>
                     @else
-                        <span style="text-align: justify !important;">Deberá proporcionar la información correspondiente a los cinco empleos, cargos o comisiones que haya tenido (en caso de que cuente con ellos).</span>
+                        <p align="justify">Deberá proporcionar la información correspondiente a los cinco empleos, cargos o comisiones que haya tenido (en caso de que cuente con ellos).</p>
                         <br>
                         <div class="alert alert-danger text-center" role="alert">
                             <label style="margin-top:10px;">
