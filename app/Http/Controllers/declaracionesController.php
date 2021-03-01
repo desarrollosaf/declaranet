92238aadd298530fcfd34121a9e2d73d93f7cf74
giss <gis_ale13@hotmail.com>
2021-03-01 15:51:51 -0600

    +

--- app/Http/Controllers/declaracionesController.php
+++ app/Http/Controllers/declaracionesController.php
@@ -2450,7 +2450,7 @@ class declaracionesController extends Controller
                                 $habitaDomicilioDeclarante = true;
                                 //   $lugarReside = $title->d
                             }
-
+dd($experiencia_pareja);
                             if ($experiencia_pareja[0]->ambito_sector_id == "1") {
                                // dd("entra uf exp");
                                 if($experiencia_pareja[0]->nivel_orden_gobierno_id == null){
