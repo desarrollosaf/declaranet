366d932a1ea3032770b6120ac57b2e78ff194625
giss <gis_ale13@hotmail.com>
2021-03-01 12:57:04 -0600

    apis

--- app/Http/Controllers/declaracionesController.php
+++ app/Http/Controllers/declaracionesController.php
@@ -2372,6 +2372,7 @@ class declaracionesController extends Controller
                                 ]),
                             );
                         } else {
+                            dd($experiencia_pareja);
                             if($experiencia_pareja->ambito_sector_id == null){
                                 $experiencia_ambito_sectores_clave = null;
                                 $experiencia_ambito_sectores_valor = null;
