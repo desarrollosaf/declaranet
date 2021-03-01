91235f24a12ececffd9ce9da520262dd8d8c7916
giss <gis_ale13@hotmail.com>
2021-03-01 17:12:45 -0600

    +

--- app/Http/Controllers/declaracionesController.php
+++ app/Http/Controllers/declaracionesController.php
@@ -3008,7 +3008,7 @@ class declaracionesController extends Controller
                         $aclaraciones_domicilio_declarante = $title->domicilio->observaciones;
                     }
 
-
+dd($title->servidor_publico->nombre);
                     $temparray [$key] = $resultados = [
                         'id' => '',
                         'metadata' => ([
