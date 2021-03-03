7dff78b9ad1ae571077df0d7a4bda17fae9cf053
oscartic98 <tic.oscarrom@gmail.com>
2021-03-03 14:26:39 -0600

    dd array

--- resources/views/datosParejaDeclarante/form.blade.php
+++ resources/views/datosParejaDeclarante/form.blade.php
@@ -512,7 +512,7 @@
                         html += '    <td>' + partObj.ambito + '</td>';
                         html += '    <td class="text-center">';
                         console.log(partObj.ambito_sector_id);
-                        if (partObj.ambito_sector_id === 1) {
+                        if (parseInt(partObj.ambito_sector_id) === 1) {
                             html += '       <strong>NOMBRE:</strong>' + partObj.ente_publico + '<br>';
                             html += '       <strong>CARGO/PUESTO:</strong>' + partObj.cargo_comision + '<br>';
                             html += '       <strong>PERIODO:</strong>' + partObj.fecha_ingreso + '<br>';
