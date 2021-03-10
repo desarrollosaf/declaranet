5b01d01cdca733947a3325e08c46b5a85f75dab4
oscartic98 <tic.oscarrom@gmail.com>
2021-03-10 09:52:15 -0600

    Modificacion viajes softDeletes

--- app/Viajes.php
+++ app/Viajes.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Viajes extends Model
 {
+	use SoftDeletes;
     protected $table = "viajes";
     protected $guarded = ["id", "created_at", "updated_at", "deleted_at"];
 
