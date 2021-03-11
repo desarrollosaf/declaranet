c1f65f4c5d6c341b64b93a608d4b59ff85272f83
giss <gis_ale13@hotmail.com>
2021-03-11 12:45:11 -0600

    api

--- app/Http/Controllers/declaracionesController.php
+++ app/Http/Controllers/declaracionesController.php
@@ -552,7 +552,7 @@ class declaracionesController extends Controller
                         foreach ($title->bienes_inmuebles as $inmuebles) {
                             $aclaraciones_bienesImuebles = $aclaraciones_bienesImuebles . ' ' . $inmuebles->observaciones;
                             $domicilio_inmueble = $inmuebles->domicilio;
-                            if ($domicilio_inmueble->asentamiento_id == null) {
+                            if ($domicilio_inmueble->entidad_id == null) {
                                 $colonia = $domicilio_inmueble->colonia;
                             } else {
                                 $colonia = $domicilio_inmueble->asentamiento->valor;
